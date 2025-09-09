c6249b3e7e347a2ea29669a5d95684c2794cc521
arosyidg <rosyidkeren164@gmail.com>
2025-09-10 07:41:07 +0800

    improve jumping

--- Assets/Scripts/Ninja/States/JumpState.cs
+++ Assets/Scripts/Ninja/States/JumpState.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Ninja.FSM
@@ -7,15 +8,23 @@ namespace Ninja.FSM
 
         float maxGravity = 10.0f;
         float baseGravity = 1.0f;
+        bool isJumpingUp = false;
         public JumpState(NinjaController ninjaController) : base(ninjaController) { }
         public override void Enter()
         {
             ninjaController.animator.SetBool("isJumping", true);
             ninjaController.Jump();
+            isJumpingUp = true;
         }
 
         public override void Update()
         {
+            if (!ninjaController.ninjaInputReader.Jump && isJumpingUp)
+            {
+                float xVelocity = ninjaController.getRBVelocity().x;
+                ninjaController.setRBVelocity(new Vector2(xVelocity, 0));
+                isJumpingUp = false;
+            }
             if (ninjaController.getRBVelocity().y <= 0 && ninjaController.CheckGrounded())
             {
                 ninjaController.ChangeState(NinjaController.State.idleState);
@@ -31,7 +40,7 @@ namespace Ninja.FSM
             ninjaController.Flip(ninjaController.ninjaInputReader.MoveDirection);
 
             float yVelocity = ninjaController.getRBVelocity().y;
-            ninjaController.animator.SetFloat("yVelocity", yVelocity); 
+            ninjaController.animator.SetFloat("yVelocity", yVelocity);
         }
 
         public override void Exit()
